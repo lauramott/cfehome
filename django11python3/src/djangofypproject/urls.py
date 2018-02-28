7350c6fe5159e7b4cb56f7c45afd9f6ecba511e0
laura <lauramotteran94@gmail.com>
2018-02-28 20:39:46 +0000

    test

--- django11python3/src/djangofypproject/urls.py
+++ django11python3/src/djangofypproject/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
 urlpatterns += [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^u/', include('profiles.urls', namespace='profiles')),
     url(r'^items/', include('facescan.urls', namespace='facescan')),
     url(r'^contact/', include('menu.urls', namespace='menu')),
     url(r'^myprofile/$', TemplateView.as_view(template_name='myprofile.html'), name='myprofile'),
